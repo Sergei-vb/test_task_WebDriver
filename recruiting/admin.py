158af2e70e28cfd504d895b4de5afa25202027a8
Sergei Bogolepov <bogolepovsergei1@gmail.com>
2018-06-22 10:55:51 +0300

    Create admin interface for models.

--- recruiting/admin.py
+++ recruiting/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Company, Image, City, Vacancy, ExchangeResult
 
-# Register your models here.
+admin.site.register(Company)
+admin.site.register(Image)
+admin.site.register(City)
+admin.site.register(Vacancy)
+admin.site.register(ExchangeResult)
